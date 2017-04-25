e861466d5a6687ba4d011145dd12f619165e5c58
mmmaxou <mmmaxou@yahoo.fr>
2017-04-25 23:14:29 +0200

    db change

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-var DEPLOY = true
+var DEPLOY = true;
 var DIRECT_CONNECT = true;
-var mongojs, db;
+var db, url;
+var MongoClient = require('mongodb').MongoClient
+var assert = require('assert')
 if (!DEPLOY) {
     // Off Line
-    mongojs = require('mongojs')
-    db = mongojs('localhost:27017/mastory', ['account', 'story'])
+    url = 'mongodb://localhost:27017/mastory';
+
 } else {
     // ON line
     var mLabUser = "mmmaxou"
     var mLabpass = "azeazeaze1"
-    mongojs = require('mongojs')
-    db = mongojs('mongodb://' + mLabUser + ':' + mLabpass + '@ds117931.mlab.com:17931/mastory', ['account', 'story'])
-
+    url = 'mongodb://' + mLabUser + ':' + mLabpass + '@ds117931.mlab.com:17931/mastory';
 }
+
+MongoClient.connect(url, function (err, database) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    db = database;
+
+    initSockets()
+});
 
 //init
 var express = require('express')
@@ -69,29 +77,36 @@ var addUser = function (data, callback) {
     callback()
 }
 
-var io = require('socket.io')(serv, {})
-io.sockets.on('connection', function (socket) {
+var initSockets = function () {
 
-    var id = Math.random()
-    SOCKET_LIST[id] = socket
+    var io = require('socket.io')(serv, {})
+    io.sockets.on('connection', function (socket) {
 
-    // Send the whole Database
-    emitDB(socket)
+        var id = Math.random()
+        SOCKET_LIST[id] = socket
 
-    // Send the last entry to all
-    socket.on("entry", function (data) {
+        // Send the whole Database
+        emitDB(socket)
 
-        db.story.insert({
-            text: data
+        // Send the last entry to all
+
+        socket.on("entry", function (data) {
+
+            var collection = db.collection("story");
+            insertDocuments(collection, {
+                text: data
+            })
+            emitToAll(data)
+
         })
-        emitToAll(data)
 
     })
 
-})
-
+}
 var emitDB = function (socket) {
-    findDocuments("story", function (data) {
+
+    var collection = db.collection("story")
+    findDocuments(collection, function (data) {
 
         socket.emit("init", data)
 
@@ -105,17 +120,21 @@ var emitToAll = function (data) {
     }
 
 }
-var findDocuments = function (selector, callback) {
-    // Get the documents collection
-    var collection = db.collection(selector);
+var findDocuments = function (collection, callback) {
     // Find some documents
     collection.find({}).toArray(function (err, docs) {
         if (err) {
             console.error("Error : " + err)
             return
         }
-        console.log("Found the following records");
-        console.log(docs)
+        console.log("Found the records");
         callback(docs);
+    });
+}
+var insertDocuments = function (collection, data) {
+    // Insert some documents
+    collection.insert(data, function (err, result) {
+        assert.equal(err, null);
+        console.log("Inserted the data into the collection");
     });
 }
